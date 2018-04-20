cfb9bfc8eb14c0a3651298c2d5514b89493ce6bf
AlecGMain <geosimonianalec@gmail.com>
2018-04-19 17:27:55 -0700

    Remove function complete. Began working on RemoveAt function.

--- SingleyLinkedList/SingleyLinkedList.cs
+++ SingleyLinkedList/SingleyLinkedList.cs
@@ -42,34 +42,61 @@ namespace SingleyLinkedList
                     tempNode = tempNode.Next;
                 }
                 tempNode.Next = new Node<T>(Value);
-                
+
             }
         }
         public bool Remove(T Value)
         {
             //add a check to see if the value is in the list before doing anything else...
             Node<T> temp = head;
-            
+            bool exists = false;
+
             while (temp.Next != null)
             {
-                temp = temp.Next;
-            }
-            if (head.Value.CompareTo(Value) == 0)
-            {
-                head = head.Next;
-                return true;
-            }
-            else
-            { 
-          
-                bool found = false;
-                Node<T> current = head;
-                while (current.Next.Value.CompareTo(Value) != 0)
+                if (temp.Value.CompareTo(Value) == 0)
                 {
-
-                    current = current.Next;
+                    
+                    exists = true;
                 }
 
+                temp = temp.Next;
+            }
+
+            if (head.Value.CompareTo(Value) == 0)
+            {
+                head = head.Next; //technically doesn't change the structure but since there is no way to reach the original head it is cleaned up by the garbage collecter
+                return true;
+            }
+            if (exists != true)
+            {
+                return false;
+            }
+
+            else
+            {
+                Node<T> current = head;
+
+                while (current.Next != null)
+                {
+                    if (current.Next.Value.CompareTo(Value) == 0)
+                    {
+                        current.Next = current.Next.Next;
+                        head = current;//changes the structure
+                        return true;
+                    }
+                    current = current.Next; //does not change the structure
+                }
+                return false;
+
+            }
+
+        }
+        public void RemoveAt(int Index)
+        {
+            Node<T> current = head;
+            for(int i = 0; i < Index; i++)
+            {
+                current = current.Next;
             }
         }
     }
