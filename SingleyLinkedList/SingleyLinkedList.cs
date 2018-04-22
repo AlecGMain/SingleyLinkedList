84e8c2b33ef394f0810798d79952a8c61688efd8
GMRDL4 <gmrstudent@outlook.com>
2018-04-22 14:31:00 -0700

    Completed RemoveAt, IsEmpty, Clear, and Contains functions. Worked on Count function.

--- SingleyLinkedList/SingleyLinkedList.cs
+++ SingleyLinkedList/SingleyLinkedList.cs
@@ -91,13 +91,140 @@ namespace SingleyLinkedList
             }
 
         }
-        public void RemoveAt(int Index)
+        public bool RemoveAt(int Index)
         {
-            Node<T> current = head;
-            for(int i = 0; i < Index; i++)
+            if (head == null)
             {
-                current = current.Next;
+                return false;
+            }
+            
+            if (Index == 0)
+            {
+                head = head.Next;
+                return true;
+            }
+            else if (Index > 0)
+            {
+                Node<T> current = head;
+                for (int i = 0; i < Index - 1; i++)
+                {
+                    if (current.Next != null)
+                    {
+                        current = current.Next;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                if (current.Next != null)
+                {
+                    current.Next = current.Next.Next;
+                    head = current;
+                    return true; 
+                }
+                else
+                {
+                    return false;
+                }
+           
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        public bool IsEmpty()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
+        public void Clear()
+        {
+            head = null;
+        }
+        public bool Contains(T Value)
+        {
+            bool contains = false;
+            Node<T> current = head;
+            if (head.Next == null)
+            {
+                if (head.Value.CompareTo(Value) == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            while (current.Next != null)
+            {
+                if(current.Next.Value.CompareTo(Value) == 0)
+                {
+                    return true;
+                }
+                
+                current = current.Next;
+            }
+            return contains;
+        }
+        public Node<T> Find(T Value)
+        {
+            if (head.Next == null)
+            {
+                if (head.Value.CompareTo(Value) == 0)
+                {
+                    return head;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            Node<T> current = head;
+            while (current.Next != null)
+            {
+                if (current.Next.Value.CompareTo(Value) == 0)
+                {
+                    return current;
+                }
+
+                current = current.Next;
+            }
+            return null;
+        }
+        public int Count()
+        {
+            if(head == null)
+            {
+                return 0;
+            }
+            else if (head.Next == null)
+            {
+                return 1;
+            }
+            else
+            {
+                int count = 1;
+                while (current.Next != null)
+                {
+                    if (current.Next.Value.CompareTo(Value) == 0)
+                    {
+                        return current;
+                    }
+
+                    current = current.Next;
+                }
+            }
+        }
+    }
     }
 }
